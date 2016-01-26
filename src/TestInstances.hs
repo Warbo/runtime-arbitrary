aeb0b6408195a0e788b6e2cd048409b093a9bbe2
Chris Warburton <chriswarbo@gmail.com>
2016-01-26 12:07:00 +0000

    Added Ord and Typeable to Foo

--- src/TestInstances.hs
+++ src/TestInstances.hs
@@ -1,8 +1,12 @@
 module TestInstances where
 
+import Data.Typeable
 import Test.QuickCheck
 
-data Foo = AFoo deriving (Show)
+data Foo = AFoo deriving (Show, Eq, Typeable)
+
+instance Ord Foo where
+  compare x y = EQ
 
 instance Arbitrary Foo where
   arbitrary = return AFoo
