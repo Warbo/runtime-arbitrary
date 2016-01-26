3c0c49cab7a93f28519a13d82d9e3f9367be61db
Chris Warburton <chriswarbo@gmail.com>
2016-01-26 11:53:50 +0000

    Added Ord and Typeable runtime constraints

--- src/RuntimeArbitrary.hs
+++ src/RuntimeArbitrary.hs
@@ -6,7 +6,13 @@ import Data.Typeable
 import IfCxt
 import Test.QuickCheck
 
-getArbGen :: forall proxy a. (Typeable a, IfCxt (Arbitrary a)) => proxy a -> [Gen a]
-getArbGen _ = ifCxt (Proxy::Proxy (Arbitrary a))
-                    [arbitrary]
+getArbGen :: forall proxy a. (IfCxt (Typeable  a),
+                              IfCxt (Arbitrary a),
+                              IfCxt (Ord       a)) => proxy a -> [Gen a]
+getArbGen _ = ifCxt               (Proxy::Proxy (Arbitrary a))
+                    (ifCxt        (Proxy::Proxy (Typeable  a))
+                           (ifCxt (Proxy::Proxy (Ord       a))
+                                  [arbitrary]
+                                  [])
+                           [])
                     []
