c1f646ce6ee9ebac407821f50b10dc9d1d03d6ac
Chris Warburton <chriswarbo@gmail.com>
2016-09-07 16:10:23 +0100

    Add instances for Nat

--- src/TestInstances.hs
+++ src/TestInstances.hs
@@ -5,10 +5,22 @@ import Test.QuickCheck
 
 data Foo = AFoo deriving (Show, Eq, Typeable)
 
-data Nat = Z | S Nat
-
 instance Ord Foo where
   compare x y = EQ
 
 instance Arbitrary Foo where
   arbitrary = return AFoo
+
+data Nat = Z | S Nat deriving (Show, Eq, Typeable)
+
+instance Ord Nat where
+  Z   <=   y = True
+  S x <= Z   = False
+  S x <= S y = x <= y
+
+instance Arbitrary Nat where
+  arbitrary = toNat <$> arbitrary
+
+toNat :: Integer -> Nat
+toNat 0 = Z
+toNat n = S (toNat (abs n - 1))
